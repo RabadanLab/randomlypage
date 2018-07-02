873a2514b0fa0f644abac20ee7370cb353290ed2
gitliver <oe2118@cumc.columbia.edu>
2018-07-02 16:46:58 -0400

    basic working setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+      inline: true,
+      port: 8008
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
